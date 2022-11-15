81edd8befc096de012516ada94983f898e0d18bd
Tada Yoshihara <tada3@outlook.com>
2022-11-15 17:06:41 +0900

    added comments

--- 02_ingest/montly_update/01_setup_svc_acct.sh
+++ 02_ingest/montly_update/01_setup_svc_acct.sh
@@ -3,6 +3,7 @@
 PROJECT_ID=$(gcloud config get-value project)
 BUCKET=${PROJECT_ID}-biwako
 REGION=asia-southeast1
+DATASET_NAME=kura
 SVC_ACCT=svc-monthly-ingest
 SVC_PRINCIPAL=serviceAccount:${SVC_ACCT}@${PROJECT_ID}.iam.gserviceaccount.com
 
@@ -20,11 +21,18 @@ gsutil iam ch ${SVC_PRINCIPAL}:roles/storage.admin gs://$BUCKET
 
 
 # ability to create/delete partitions etc in BigQuery table
+#bq --project_id=${PROJECT_ID} query --nouse_legacy_sql \
+#  "GRANT \`roles/bigquery.dataOwner\` ON SCHEMA \`$PROJECT_ID.$DATASET_NAME\` TO '$SVC_PRINCIPAL' "
+# Somehow this query gets error:
+#   Not found: Dataset mikan-mochi:kura
+# Explicitly adding project_id does not work. So as temporary fix, I used console to grant
+# priviledge to the service account.
+# Sharing -> Permissions -> Add Principal
 bq --project_id=${PROJECT_ID} query --nouse_legacy_sql \
-  "GRANT \`roles/bigquery.dataOwner\` ON SCHEMA kura TO '$SVC_PRINCIPAL' "
+  "GRANT \`roles/bigquery.dataOwner\` ON SCHEMA \`$DATASET_NAME\` TO '$SVC_PRINCIPAL' "
 
+# You can see the assigned role in IAM -> (Select Project) -> Permissions
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
   --member ${SVC_PRINCIPAL} \
   --role roles/bigquery.jobUser
 
-  
